1f3ca387077c8835c91ce46ee482cfba3af961b7
anastasiiya1 <shapoval.anastasiiya@gmail.com>
2024-01-09 14:15:16 +0200

    3 tasks added

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,24 @@
-function checkForSpam(message){
-    const blackListedWord1 = "spam";
-    const blackListedWord2 = "sale";
+const profile = {
+    username: 'Jacob',
+    playTime: 300,
+    changeUsername(newName) {
+       this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+}
+}
 
-    const transformedString1 = blackListedWord1.toUpperCase();
-    const transformedString2 = blackListedWord2.toUpperCase();
 
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-    if (message.toUpperCase().includes(transformedString1) || message.toUpperCase().includes(transformedString2)){
-        return true;
-    } else{
-        return false;
-    }
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-}console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+  
